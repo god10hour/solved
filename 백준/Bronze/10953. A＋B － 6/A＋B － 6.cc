9b4ab1e950400a06dd32387659e7ba26637f7dc4
god10hour <god10hour@gmail.com>
2025-10-20 12:19:28 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // cin.tie(0)->sync_with_stdio(0);
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int a, b;
+        scanf("%d,%d", &a, &b);
+        printf("%d\n", a+b);
+    }
+}
