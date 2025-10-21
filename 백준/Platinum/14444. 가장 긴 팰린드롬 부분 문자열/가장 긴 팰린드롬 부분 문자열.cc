9a5bee0025b276429dde86fd5986fa32a137f479
god10hour <god10hour@gmail.com>
2025-10-21 17:47:48 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 3500 KB -BaekjoonHub

---
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> manacher(string s, int n) {
+    int p=0, r=0;
+    vector<int> v(n);
+    for (int i=0; i<n; i++) {
+        if (i<=r)
+            v[i]=min(v[p*2-i], r-i);
+        else v[i]=0;
+        while (i-v[i]>0 && i+v[i]<n-1 && s[i-v[i]-1]==s[i+v[i]+1])
+            v[i]++;
+        if (r<i+v[i])
+            r=i+v[i], p=i;
+    }
+    return v;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string s, t;
+    cin >> t;
+    for (int i=0; i<t.length(); i++)
+        s+='.', s+=t[i];
+    s+='.';
+    auto v=manacher(s, s.length());
+    int r=-1;
+    for (int i=0; i<s.size(); i++)
+        r=max(r, v[i]);
+    cout << r;
+}
