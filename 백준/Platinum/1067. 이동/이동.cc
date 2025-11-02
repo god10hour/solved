9bc9fb29a5a1846d8c055ab68c895380574bead0
god10hour <god10hour@gmail.com>
2025-11-03 01:34:30 +0900

    [Platinum I] Title: 이동, Time: 56 ms, Memory: 8012 KB -BaekjoonHub

--- 백준/Platinum/1067. 이동/이동.cc
+++ 백준/Platinum/1067. 이동/이동.cc
@@ -1,69 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const double PI=acos(-1);
-typedef complex<double> cpx;
 
-void FFT(vector<cpx> &v, bool inv) {
-    int s=v.size();
-    for (int i=1, j=0; i<s; i++) {
-        int b=s/2;
-        while (j>=b)
-            j-=b, b/=2;
-        j+=b;
-
-        if (i<j)
-            swap(v[i], v[j]);
+#define int int64_t
+template<int mod, int root>
+struct NTT {
+    static int pow(int n, int k) {
+        int r=1;
+        while (k) {
+            if (k&1)
+                r=r*n%mod;
+            n=n*n%mod;
+            k/=2;
+        }
+        return r;
     }
 
-    for (int k=1; k<s; k*=2) {
-        double ang=inv?PI/k:-PI/k;
-        cpx w(cos(ang), sin(ang));
-
-        for (int i=0; i<s; i+=k*2) {
-            cpx uni(1, 0);
-            for (int j=0; j<k; j++) {
-                cpx a=v[i+j], b=v[i+j+k]*uni;
-                v[i+j]=a+b;
-                v[i+j+k]=a-b;
-                uni*=w;
+    static void ntt(vector<int> &v, bool inv) {
+        int s=v.size();
+        for (int i=1, j=0; i<s; i++) {
+            int b=s/2;
+            while (j>=b)
+                j-=b, b/=2;
+            j+=b;
+            if (i<j) swap(v[i], v[j]);
+        }
+        for (int k=1; k<s; k*=2) {
+            int w=pow(root, (mod-1)/(k*2));
+            if (inv) w=pow(w, mod-2);
+            for (int i=0; i<s; i+=k*2) {
+                int uni=1;
+                for (int j=0; j<k; j++) {
+                    int a=v[i+j];
+                    int b=v[i+j+k]*uni%mod;
+                    v[i+j]=(a+b)%mod;
+                    v[i+j+k]=(a-b+mod)%mod;
+                    uni=uni*w%mod;
+                }
             }
+        }
+        if (inv) {
+            int I=pow(s, mod-2);
+            for (int i=0; i<s; i++)
+                v[i]=v[i]*I%mod;
         }
     }
 
-    if (inv)
+    static vector<int> conv(vector<int> V, vector<int> U) {
+        if (V.empty() || U.empty())return {};
+        int s=1, n=V.size()+U.size()-1;
+        while (s<n) s*=2;
+        for (auto &x: V) { x%=mod; if (x<0) x+=mod; }
+        for (auto &x: U) { x%=mod; if (x<0) x+=mod; }
+        V.resize(s); ntt(V, 0);
+        U.resize(s); ntt(U, 0);
         for (int i=0; i<s; i++)
-            v[i]/=s;
-}
+            V[i]=V[i]*U[i]%mod;
+        ntt(V, 1), V.resize(n);
+        return V;
+    }
+};
 
-vector<cpx> mul(vector<cpx> &v, vector<cpx> &u) {
-    int s=2;
-    while (s<v.size()+u.size())
-        s*=2;
-    v.resize(s), FFT(v, 0);
-    u.resize(s), FFT(u, 0);
-
-    vector<cpx> w(s);
-    for (int i=0; i<s; i++)
-        w[i]=v[i]*u[i];
-    FFT(w, 1);
-    return w;
-}
-
-main() {
+int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
-    int n, k;
+    int n, s=0;
     cin >> n;
-    vector<cpx> v(n*2), u(n);
-
-    for (int i=0; i<n; i++)
-        cin >> k, v[i]=v[i+n]=cpx(k, 0);
-    for (int i=0; i<n; i++)
-        cin >> k, u[n-i-1]=cpx(k, 0);
-    vector<cpx> w=mul(v, u);
-    int s=0;
-    for (int i=0; i<w.size(); i++)
-        s=max(s, (int)round(w[i].real()));
-
-    cout << s;
+    vector<int> V(n*2), U(n);
+    for (int i=0; i<n; i++) cin >> V[i], V[i+n]=V[i];
+    for (int i=0; i<n; i++) cin >> U[n-i-1];
+    auto w=NTT<1224736769, 3>::conv(V, U);
+    for (int i: w) s=max(s, i);
+    cout << s << '\n';
 }
