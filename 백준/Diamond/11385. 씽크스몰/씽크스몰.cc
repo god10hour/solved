988e8ecac03f8248a0cfd5e58ff633c821423292
god10hour <god10hour@gmail.com>
2025-11-03 01:40:51 +0900

    [Diamond IV] Title: 씽크스몰, Time: 4616 ms, Memory: 145844 KB -BaekjoonHub

--- 백준/Diamond/11385. 씽크스몰/씽크스몰.cc
+++ 백준/Diamond/11385. 씽크스몰/씽크스몰.cc
@@ -1,76 +1,142 @@
 #include <bits/stdc++.h>
-#define int int64_t
 using namespace std;
-const double PI=acos(-1);
-typedef complex<long double> cpx;
 
-void FFT(vector<cpx> &v, bool inv) {
-    int s=v.size();
-    for (int i=1, j=0; i<s; i++) {
-        int b=s/2;
-        while (j>=b)
-            j-=b, b/=2;
-        j+=b;
-        if (i<j) swap(v[i], v[j]);
+#define int int64_t
+template<int mod, int root>
+struct NTT {
+    static int pow(int n, int k) {
+        int r=1;
+        while (k) {
+            if (k&1)
+                r=r*n%mod;
+            n=n*n%mod;
+            k/=2;
+        }
+        return r;
     }
-    for (int k=1; k<s; k*=2) {
-        long double ang=inv?PI/k:-PI/k;
-        cpx w(cos(ang), sin(ang));
-        for (int i=0; i<s; i+=k*2) {
-            cpx uni(1, 0);
-            for (int j=0; j<k; j++) {
-                cpx a=v[i+j], b=v[i+j+k]*uni;
-                v[i+j]=a+b;
-                v[i+j+k]=a-b;
-                uni*=w;
+
+    static void ntt(vector<int> &v, bool inv) {
+        int s=v.size();
+        for (int i=1, j=0; i<s; i++) {
+            int b=s/2;
+            while (j>=b)
+                j-=b, b/=2;
+            j+=b;
+            if (i<j) swap(v[i], v[j]);
+        }
+        for (int k=1; k<s; k*=2) {
+            int w=pow(root, (mod-1)/(k*2));
+            if (inv) w=pow(w, mod-2);
+            for (int i=0; i<s; i+=k*2) {
+                int uni=1;
+                for (int j=0; j<k; j++) {
+                    int a=v[i+j];
+                    int b=v[i+j+k]*uni%mod;
+                    v[i+j]=(a+b)%mod;
+                    v[i+j+k]=(a-b+mod)%mod;
+                    uni=uni*w%mod;
+                }
             }
         }
+        if (inv) {
+            int I=pow(s, mod-2);
+            for (int i=0; i<s; i++)
+                v[i]=v[i]*I%mod;
+        }
     }
-    if (inv) for (int i=0; i<s; i++) v[i]/=s;
-}
 
-vector<int> CVL(vector<int>& V, vector<int>& U) {
-    vector<cpx> v(V.begin(), V.end()), u(U.begin(), U.end());
-    int s=2;
-    while (s<v.size()+u.size()) s*=2;
-    v.resize(s), FFT(v, 0);
-    u.resize(s), FFT(u, 0);
-    vector<cpx> w(s);
-    vector<int> r(s);
-    for (int i=0; i<s; i++)
-        w[i]=v[i]*u[i];
-    FFT(w, 1);
-    for (int i=0; i<s; i++)
-        r[i]=round(w[i].real());
-    return r;
-}
+    static vector<int> conv(vector<int> V, vector<int> U) {
+        if (V.empty() || U.empty())return {};
+        int s=1, n=V.size()+U.size()-1;
+        while (s<n) s*=2;
+        for (auto &x: V) { x%=mod; if (x<0) x+=mod; }
+        for (auto &x: U) { x%=mod; if (x<0) x+=mod; }
+        V.resize(s); ntt(V, 0);
+        U.resize(s); ntt(U, 0);
+        for (int i=0; i<s; i++)
+            V[i]=V[i]*U[i]%mod;
+        ntt(V, 1), V.resize(n);
+        return V;
+    }
+};
+
+struct CRT {
+    using u128=__uint128_t;
+    static constexpr int MODS[3]={
+        167772161, 469762049, 1224736769
+    };
+    static constexpr int ROOT=3;
+    using P0=NTT<MODS[0], ROOT>;
+    using P1=NTT<MODS[1], ROOT>;
+    using P2=NTT<MODS[2], ROOT>;
+
+    static int pow(int n, int k, int mod) {
+        int r=1;
+        while (k) {
+            if (k&1)
+                r=r*n%mod;
+            n=n*n%mod;
+            k/=2;
+        }
+        return r;
+    }
+
+    static int combine(const int a[3]) {
+        u128 r=0, mod=1;
+        for (int i=0; i<3; i++) {
+            int Mi=MODS[i], ri=r%Mi;
+            if (ri<0) ri+=Mi;
+            int diff=(a[i]+Mi-ri)%Mi;
+            int inv=pow(mod%Mi, Mi-2, Mi);
+            r+=mod*(diff*inv%Mi);
+            mod*=Mi;
+        }
+        return r;
+    }
+
+    static vector<int> conv(const vector<int> &V, const vector<int> &U) {
+        if (V.empty() || U.empty()) return {};
+        int n=V.size()+U.size()-1;
+        array<vector<int>, 3> v, u;
+        for (int i=0; i<3; i++) v[i].resize(V.size());
+        for (int i=0; i<3; i++) u[i].resize(U.size());
+
+        for (int i=0; i<V.size(); i++)
+            for (int j=0; j<3; j++) {
+                v[j][i]=V[i]%MODS[j];
+                if (v[j][i]<0) v[j][i]+=MODS[j];
+            }
+        for (int i=0; i<U.size(); i++)
+            for (int j=0; j<3; j++) {
+                u[j][i]=U[i]%MODS[j];
+                if (u[j][i]<0) u[j][i]+=MODS[j];
+            }
+
+        auto w0=P0::conv(v[0], u[0]);
+        auto w1=P1::conv(v[1], u[1]);
+        auto w2=P2::conv(v[2], u[2]);
+
+        vector<int> r(n);
+        for (int i=0; i<n; i++) {
+            int a[3]={
+                i<w0.size()? w0[i]:0,
+                i<w1.size()? w1[i]:0,
+                i<w2.size()? w2[i]:0
+            };
+            r[i]=combine(a);
+        }
+        return r;
+    }
+};
 
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, m, r=0;
     cin >> n >> m;
-    vector<int> v(n+1), u(m+1), V, U, w11, w12, w21, w22;
-    for (auto& i:v) cin >> i;
-    for (auto& i:u) cin >> i;
-
-    V=v, U=u;
-    for (auto& i:V) i&=(1<<10)-1;
-    for (auto& i:U) i&=(1<<10)-1;
-    w22=CVL(V, U);
-    V=v, U=u;
-    for (auto& i:V) i>>=10;
-    for (auto& i:U) i&=(1<<10)-1;
-    w12=CVL(V, U);
-    V=v, U=u;
-    for (auto& i:V) i&=(1<<10)-1;
-    for (auto& i:U) i>>=10;
-    w21=CVL(V, U);
-    V=v, U=u;
-    for (auto& i:V) i>>=10;
-    for (auto& i:U) i>>=10;
-    w11=CVL(V, U);
-
-    for (int i=0; i<=n+m; i++)
-        r^=(w11[i]<<20)+((w12[i]+w21[i])<<10)+w22[i];
-    cout << r;
+    vector<int> V(n+1), U(m+1);
+    for (int i=0; i<=n; i++) cin >> V[i];
+    for (int i=0; i<=m; i++) cin >> U[i];
+    auto w=CRT::conv(V, U);
+    for (int x: w) r^=x;
+    cout << r << '\n';
 }
