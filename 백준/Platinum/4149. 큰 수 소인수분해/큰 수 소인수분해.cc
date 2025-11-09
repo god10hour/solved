37083ec72c86ce50364cba665c5d80833f392a05
god10hour <god10hour@gmail.com>
2025-11-10 08:57:12 +0900

    [Platinum I] Title: 큰 수 소인수분해, Time: 20 ms, Memory: 2028 KB, Score: 159 point -BaekjoonHub

--- 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.cc
+++ 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.cc
@@ -1,82 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-int p[12]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+using lint=uint64_t;
 
-ll GCD(ll a,ll b) {
-    return b?GCD(b, a%b):a;
-}
-
-ll Pow(__int128 a, __int128 n, __int128 p) {
+lint pow(__int128 n, __int128 k, lint mod) {
     __int128 r=1;
-    a%=p;
-    while(n) {
-        if (n%2)
-            r=r*a%p;
-        a=a*a%p;
-        n/=2;
+    while (k) {
+        if (k&1)
+            r=r*n%mod;
+        n=n*n%mod;
+        k/=2;
     }
     return r;
 }
 
-bool f(ll n, ll a) {
-    ll k=n-1;
-    while(1) {
-        ll d=Pow(a, k, n);
-        if (k%2)
-            return d==1 || d==n-1;
-        if (d==n-1)
-            return 1;
-        k/=2;
-    }
-}
+constexpr int base[]={
+    2, 3, 5, 7, 11, 13, 17
+};
+bool prime(lint n) {
+    if (n<2) return 0;
+    auto f=[](lint n, lint a)->bool {
+        lint k=n-1;
+        while (true) {
+            lint d=pow(a, k, n);
+            if (k&1) return d==1 || d==n-1;
+            if (d==n-1) return 1;
+            k/=2;
+        }
+    };
 
-bool isPrime(ll n) {
-    if (n<2)
-        return 0;
-    for (int i=0; i<12; i++) {
-        if (n==p[i])
-            return 1;
-        if (n%p[i]==0)
-            return 0;
-        if (!f(n, p[i]))
-            return 0;
+    for (const auto &p: base) {
+        if (n==p) return 1;
+        if (n%p==0) return 0;
+        if (!f(n, p)) return 0;
     }
     return 1;
 }
 
-ll div(__int128 n) {
-    if (n%2==0)
-        return 2;
-    if (isPrime(n))
-        return n;
+lint div(__int128 n) {
+    if (n%2==0) return 2;
+    if (prime(n)) return n;
 
-    __int128 x=rand()%(n-2)+2, y=x, c=rand()%10+1, g=1;
-
+    __int128 x=rand()%(n-2)+2, y=x;
+    __int128 c=rand()%10+1, g=1;
     while (g==1) {
-        x=(x*x%n+c)%n;
-        y=(y*y%n+c)%n;
-        y=(y*y%n+c)%n;
-        g=GCD(abs(x-y),n);
-        if (g==n)
-            return div(n);
+        x=(x*x+c)%n;
+        y=(y*y+c)%n;
+        y=(y*y+c)%n;
+        g=gcd(abs(x-y), n);
+        if (g==n) return div(n);
     }
-    if (isPrime(g))
-        return g;
+
+    if (prime(g)) return g;
     return div(g);
 }
 
-int main() {
-    ll n;
-    cin >> n;
-    vector<ll> s;
-
+vector<lint> factor(lint n) {
+    vector<lint> r;
     while (n>1) {
-        ll d=div(n);
-        s.push_back(d);
+        lint d=div(n);
+        r.push_back(d);
         n/=d;
     }
-    sort(s.begin(), s.end());
-    for (ll i=0; i<s.size(); i++)
-        cout << s[i] << '\n';
+    sort(r.begin(), r.end());
+    return r;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    lint n;
+    cin >> n;
+    auto a=factor(n);
+    for (auto x: a)
+        cout << x << '\n';
 }
