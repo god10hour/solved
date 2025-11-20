5f0725cb5f7473c07dbc8fa6386ac0d593660886
god10hour <god10hour@gmail.com>
2025-11-20 09:42:10 +0900

    [Platinum IV] Title: 원 영역, Time: 100 ms, Memory: 10640 KB -BaekjoonHub

---
+++ 백준/Platinum/10000. 원 영역/원 영역.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU {
+    vector<int> p;
+    explicit DSU(int n=0) { init(n); }
+    void init(int n) { p.assign(n, -1); }
+
+    int find(int x) { return p[x]<0? x: p[x]=find(p[x]); }
+    bool join(int x, int y) {
+        x=find(x), y=find(y);
+        if (x==y) return 0;
+        if (p[x]>p[y]) swap(x, y);
+        p[x]+=p[y];
+        p[y]=x;
+        return 1;
+    }
+    int size(int x) { return -p[find(x)]; }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> L(n), R(n), v;
+    for (int i=0; i<n; i++) {
+        int x, r;
+        cin >> x >> r;
+        L[i]=x-r, R[i]=x+r;
+        v.push_back(L[i]), v.push_back(R[i]);
+    }
+    ranges::sort(v);
+    v.erase(ranges::unique(v).begin(), v.end());
+
+    int V=v.size(), E=n*2, C=0;
+    DSU dsu(V);
+    for (int i=0; i<n; i++) {
+        int l=ranges::lower_bound(v, L[i])-v.begin();
+        int r=ranges::lower_bound(v, R[i])-v.begin();
+        dsu.join(l, r);
+    }
+    for (int i=0; i<V; i++) if (i==dsu.find(i)) C++;
+    cout << 1-V+E+C << '\n';
+}
