19a5a7106e0504cf7bd031d1d2f03514a9b8f67a
god10hour <god10hour@gmail.com>
2025-11-24 11:45:44 +0900

    [Platinum IV] Title: 열혈강호, Time: 72 ms, Memory: 6132 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.cc
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Bipartite Matching: O(√V*E)
+struct HopcroftKarp {
+    int Ln, Rn;
+    vector<vector<int>> g;
+    vector<int> dist;
+    vector<int> Lmatch, Rmatch;
+
+    explicit HopcroftKarp(int Ln, int Rn): Ln(Ln), Rn(Rn), g(Ln) {
+        dist.resize(Ln);
+        Lmatch.assign(Ln, -1);
+        Rmatch.assign(Rn, -1);
+    }
+
+    void add(int u, int v) {
+        g[u].push_back(v);
+    }
+
+    bool bfs() {
+        queue<int> q;
+        bool reachable=false;
+
+        for (int u=0; u<Ln; u++)
+            if (Lmatch[u]==-1)
+                dist[u]=0, q.push(u);
+            else dist[u]=-1;
+        while (!q.empty()) {
+            int u=q.front(); q.pop();
+            for (int v: g[u]) {
+                int u2=Rmatch[v];
+                if (u2!=-1 && dist[u2]==-1) {
+                    dist[u2]=dist[u]+1;
+                    q.push(u2);
+                }
+                if (u2==-1)
+                    reachable=true;
+            }
+        }
+        return reachable;
+    }
+
+    bool dfs(int u) {
+        for (int v: g[u]) {
+            int u2=Rmatch[v];
+            if (u2==-1 || (dist[u2]==dist[u]+1 && dfs(u2))) {
+                Lmatch[u]=v;
+                Rmatch[v]=u;
+                return true;
+            }
+        }
+        dist[u]=-1;
+        return false;
+    }
+
+    int max() {
+        int m=0;
+        while (bfs())
+            for (int u=0; u<Ln; u++)
+                if (Lmatch[u]==-1 && dfs(u))
+                    m++;
+        return m;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    HopcroftKarp BM(n, m);
+    for (int i=0; i<n; i++) {
+        int k, x;
+        cin >> k;
+        for (int j=0; j<k; j++) {
+            cin >> x;
+            BM.add(i, x-1);
+        }
+    }
+    cout << BM.max() << '\n';
+}
