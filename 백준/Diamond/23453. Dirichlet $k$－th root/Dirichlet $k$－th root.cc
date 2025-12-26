8a2d5f85920af814046cd6453a71e7b527edae6e
god10hour <god10hour@gmail.com>
2025-12-26 23:25:06 +0900

    [Diamond IV] Title: Dirichlet $k$-th root, Time: 52 ms, Memory: 4104 KB -BaekjoonHub

--- 백준/Diamond/23453. Dirichlet $k$－th root/Dirichlet $k$－th root.cc
+++ 백준/Diamond/23453. Dirichlet $k$－th root/Dirichlet $k$－th root.cc
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr int MOD=998244353;
 
 template<int mod>
 struct Mint {
@@ -58,69 +59,64 @@ struct Mint {
     explicit operator int32_t() const { return v; }
     explicit operator int64_t() const { return v; }
 };
-using Z=Mint<998244353>;
+using Z=Mint<MOD>;
 
-constexpr int N=1e6;
-Z f[N+5], g[N+5];
-int prs[80005], pc;
-uint8_t om[N+5];
-Z invs[32];
-bitset<N+5> sieve;
-
-int init(int n) {
-    sieve.set();
-    sieve[0]=sieve[1]=0;
-    for (int i=2; 1LL*i*i<=n; i++) if (sieve[i])
-        for (int j=i*i; j<=n; j+=i) sieve[j]=0;
-    pc=0;
-    for (int i=2; i<=n; i++) if (sieve[i]) prs[pc++]=i;
-    int M=0;
-    for (int t=0; t<pc; t++) {
-        int p=prs[t];
-        for (int x=p; x<=n; x+=p) {
-            int y=x;
-            while (y%p==0) om[x]++, y/=p;
+vector<Z> conv(const vector<Z> &v, const vector<Z> &u) {
+    int n=v.size()-1;
+    vector w(n+1, Z(0));
+    for (int i=1; i<=n; i++) {
+        if (v[i]==Z(0)) continue;
+        int lim=n/i;
+        for (int j=1; j<=lim; j++) {
+            if (u[j]==Z(0)) continue;
+            w[i*j]+=v[i]*u[j];
         }
     }
-    for (int i=2; i<=n; i++) M=max<int>(M, om[i]);
-    return M;
+    return w;
 }
 
-void lnO(Z *a, int n) {
-    for (int i=1; i<=n; i++) g[i-1]=a[i-1]*Z(om[i]);
-    for (int i=1; i<=n; i++) {
-        for (int j=2; 1LL*i*j<=n; j++)
-            g[i*j-1]-=g[i-1]*a[j-1];
-        if (i>1) g[i-1]*=invs[om[i]];
+vector<Z> dlog(const vector<Z> &v) {
+    int n=v.size()-1, L=0;
+    while (1<<(L+1)<=n) L++;
+    vector<Z> u=v;
+    u[1]-=Z(1);
+    vector<Z> p=u, r(n+1, Z(0));
+    for (int m=1; m<=L; m++) {
+        Z coef=Z(1)/Z(m);
+        if (m%2==0) coef=-coef;
+        for (int i=1; i<=n; i++) r[i]+=p[i]*coef;
+        if (m!=L) p=conv(p, u);
     }
-    for (int i=0; i<n; i++) a[i]=g[i];
+    r[1]=Z(0);
+    return r;
 }
 
-void expO(Z *a, int n) {
-    g[0]=Z(1);
-    for (int i=1; i<n; i++) g[i]=Z(0);
-    for (int i=1; i<=n; i++) a[i-1]*=Z((int)om[i]);
-    for (int i=1; i<=n; i++) {
-        if (i>1) g[i-1]*=invs[om[i]];
-        for (int j=2; 1LL*i*j<=n; j++)
-            g[i*j-1]+=g[i-1]*a[j-1];
+vector<Z> dexp(const vector<Z> &v) {
+    int n=v.size()-1, L=0;
+    while (1<<(L+1)<=n) L++;
+    vector<Z> p=v, r(n+1, Z(0));
+    r[1]=Z(1);
+    Z fac=Z(1);
+    for (int m=1; m<=L; m++) {
+        fac*=Z(m);
+        Z ifac=inv(fac);
+        for (int i=1; i<=n; i++) r[i]+=p[i]*ifac;
+        if (m!=L) p=conv(p, v);
     }
-    for (int i=0; i<n; i++) a[i]=g[i];
+    r[1]=Z(1);
+    return r;
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    int n, k;
+    int64_t n, k;
     cin >> n >> k;
-    for (int i=0; i<n; i++) cin >> f[i];
-    int M=init(n);
+    vector<Z> g(n+1);
+    for (int i=1; i<=n; i++) cin >> g[i];
+    Z kinv=inv(Z(k));
+    vector<Z> lg=dlog(g);
+    for (int i=1; i<=n; i++) lg[i]*=kinv;
+    vector<Z> f=dexp(lg);
 
-    invs[1]=Z(1);
-    for (int i=2; i<=M; i++) invs[i]=inv(Z(i));
-
-    Z ik=inv(Z(k));
-    lnO(f, n);
-    for (int i=0; i<n; i++) f[i]*=ik;
-    expO(f, n);
-    for (int i=0; i<n; i++) cout << f[i] << ' ';
+    for (int i=1; i<=n; i++) cout << f[i] << ' ';
 }
