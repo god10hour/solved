6e47c0f4615177599761744f01078747f6cf0dca
god10hour <god10hour@gmail.com>
2026-02-22 00:01:57 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    cout << fixed << setprecision(6);
+    long double r;
+    cin >> r;
+    cout << M_PI*r*r << '\n' << 2*r*r << '\n';
+}
